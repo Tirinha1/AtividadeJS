d4090077e704acd11e9f262b56c436113c591ba1
Tirinha1 <gabrielssantos702@gmail.com>
2024-06-16 11:54:33 -0300

    city table

--- cidades/script.js
+++ cidades/script.js
@@ -4,12 +4,19 @@ fetch('./cidades-mg.json')
     .catch(error => console.log(error.message))
 
 function verificarCidades(jsonData){
+    var tabela = "<table><tr><th>Nome da cidade</th><th>Codigo da Cidade</th></tr>"
     for(cidadeData of jsonData){
         let index = cidadeData.search(":");
         var codigoCidade = cidadeData.slice(0, index);
         var nomeCidade = cidadeData.slice(index+1, jsonData.lenght)
 
-        console.log(codigoCidade, nomeCidade);
+        var linha = "<tr>";
+        linha += "<td>" + nomeCidade + "</td><td>" + codigoCidade + "</td></tr>"
+
+        tabela += linha;
     }
-    
+
+    tabela += "</table>"
+
+    document.getElementById("table").innerHTML += tabela;
 }
